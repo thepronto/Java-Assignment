21cce40b52254ec16ac74dee81a689f3b38aadeb
Sriharinadha Savaram <118372340+thepronto@users.noreply.github.com>
2025-07-30 10:17:47 +0530

    Update UserInputOutput.java

--- UserInputOutput.java
+++ UserInputOutput.java
@@ -5,14 +5,19 @@ public class UserInputOutput {
         Scanner scanner = new Scanner(System.in);
 
         // Input
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
 
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Calculation
+        double sum = num1 + num2;
+        double average = sum / 2;
 
         // Output
-        System.out.println("Hello " + name + ", you are " + age + " years old.");
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+        System.out.println("The average of " + num1 + " and " + num2 + " is " + average);
 
         scanner.close();
     }
