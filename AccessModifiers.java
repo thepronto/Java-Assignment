24c37415d69167d1c7ebb8f9b8adcb472ad57c80
Sriharinadha Savaram <118372340+thepronto@users.noreply.github.com>
2025-07-30 10:13:16 +0530

    Update AccessModifiers.java

--- AccessModifiers.java
+++ AccessModifiers.java
@@ -1,12 +1,17 @@
 class DemoAccess {
-    public int pubVar = 1;
-    private int privVar = 2;
-    static int staticVar = 3;
+    public int publicVariable = 1;
+    private int privateVariable = 2;
+    static int staticVariable = 3;
 
     public void show() {
-        System.out.println("Public variable: " + pubVar);
-        System.out.println("Private variable: " + privVar);
-        System.out.println("Static variable: " + staticVar);
+        System.out.println("Public variable: " + publicVariable);
+        System.out.println("Private variable: " + privateVariable);
+        System.out.println("Static variable: " + staticVariable);
+    }
+
+    public void showContributorInfo() {
+        System.out.println("Srihari");
+        System.out.println("Accessing variables with different access modifiers.");
     }
 }
 
@@ -14,6 +19,7 @@ public class AccessModifiers {
     public static void main(String[] args) {
         DemoAccess obj = new DemoAccess();
         obj.show();
-        System.out.println("Accessing static variable: " + DemoAccess.staticVar);
+        System.out.println("Accessing static variable: " + DemoAccess.staticVariable);
+        obj.showContributorInfo();
     }
 }
