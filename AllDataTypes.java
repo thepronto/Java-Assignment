dda0433ce97d4b84d6e28bcf30a1192731aa477f
Sriharinadha Savaram <118372340+thepronto@users.noreply.github.com>
2025-07-30 10:14:50 +0530

    Update AllDataTypes.java

--- AllDataTypes.java
+++ AllDataTypes.java
@@ -1,23 +1,26 @@
 public class AllDataTypes {
     public static void main(String[] args) {
-        byte b = 100;
-        short s = 10000;
-        int i = 123456;
-        long l = 123456789L;
-        float f = 10.5f;
-        double d = 123.456;
-        char c = 'A';
-        boolean bool = true;
-        String str = "Hello, DataTypes!";
+        byte myByte = 100;
+        short myShort = 10000;
+        int myInt = 123456;
+        long myLong = 123456789L;
+        float myFloat = 10.5f;
+        double myDouble = 123.456;
+        char myChar = 'A';
+        boolean myBool = true;
+        String myString = "Hello, Srihari!";
 
-        System.out.println("byte: " + b);
-        System.out.println("short: " + s);
-        System.out.println("int: " + i);
-        System.out.println("long: " + l);
-        System.out.println("float: " + f);
-        System.out.println("double: " + d);
-        System.out.println("char: " + c);
-        System.out.println("boolean: " + bool);
-        System.out.println("String: " + str);
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("char: " + myChar);
+        System.out.println("boolean: " + myBool);
+        System.out.println("String: " + myString);
+
+        // Unique personal element inside the code
+        System.out.println("Sriharinadha S");
     }
 }
